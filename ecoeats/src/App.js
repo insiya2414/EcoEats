c4f7d4c61032d2d6104f03b35954eec6f2d6a9e5
Alyana Imperial <allyimperial@gmail.com>
2024-11-30 17:10:34 -0600

    deleted ingredients/getstarted.js (not needed)

--- ecoeats/src/App.js
+++ ecoeats/src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './Components/Layout';
 import Homepage from './Pages/homepage';
-import GetStarted from './Pages/getstarted';
-import Ingredients from './Pages/ingredients';
 import Login from './Pages/login';
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
@@ -19,7 +17,6 @@ function App() {
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Homepage />} />
                     <Route path="get-started" element={<Login />} />
-                    <Route path="ingredients" element={<Ingredients />} />
                     <Route path="login" element={<Login />} />
                     <Route path="dashboard" element={<Dashboard />} />
                     <Route path="/scan-receipt" element={<ScanReceipt />} />
